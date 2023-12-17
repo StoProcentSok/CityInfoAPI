0a604ca0752922ffc9425c6c48e9c5a187ed6a1b
Michal <michalbuczek00@gmail.com>
2023-12-17 14:04:01 +0100

    Added POIs to cityDto. Returning child properties with request.

---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,42 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterests(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{poiId}")]
+        public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int poiId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var poi = city.PointsOfInterest.FirstOrDefault(poi => poi.Id == poiId);
+            if (poi == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(poi);
+        }
+    }
+}
