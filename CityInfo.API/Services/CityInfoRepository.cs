50f4538894503ff567f9d67fc56333bb47a613b3
Michal <michalbuczek00@gmail.com>
2024-01-07 11:46:10 +0100

    Created repository for cityInfo getting cities and POIs

---
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,40 @@
+﻿using CityInfo.API.DbContexts;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.Services
+{
+    public class CityInfoRepository : ICItyInfoRepository
+    {
+        private readonly CityInfoContext context;
+
+        public CityInfoRepository(CityInfoContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public async Task<IEnumerable<City>> GetCitiesAsync()
+        {
+            return await context.Cities.OrderBy(c => c.Name).ToListAsync();
+        }
+
+        public async Task<City?> GetCityAsync(int cityId, bool includePOIs)
+        {
+            if (includePOIs)
+            {
+                return await context.Cities.Include(c => c.PointOfInterests).Where(c => c.Id == cityId).FirstOrDefaultAsync();
+            }
+
+            return await context.Cities.Where(c => c.Id == cityId).FirstOrDefaultAsync();
+        }
+
+        public async Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int poiId)
+        {
+            return await context.PointsOfInterests.Where(poi => poi.Id == poiId && poi.CityId == cityId).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<PointOfInterest>> GetPointsOfInterestsForCityAsync(int cityId)
+        {
+            return await context.PointsOfInterests.Where(poi => poi.CityId == cityId).ToListAsync();
+        }
+    }
+}
---
+++ CityInfo.API/Services/ICItyInfoRepository.cs
@@ -0,0 +1,14 @@
+﻿using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public interface ICItyInfoRepository
+    {
+        Task<IEnumerable<City>> GetCitiesAsync();
+        Task<City?> GetCityAsync(int cityId, bool includePOIs);
+
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestsForCityAsync(int cityId);
+
+        Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int poiId);
+    }
+}
