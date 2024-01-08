8cecf852da033cb0c59ddd91bf9ba97c556f8331
Michal <michalbuczek00@gmail.com>
2024-01-08 13:04:20 +0100

    Created map profiles for city and poi for DTOs

---
+++ CityInfo.API/MapProfiles/CityProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace CityInfo.API.MapProfiles
+{
+    public class CityProfile : Profile
+    {
+        public CityProfile()
+        {
+            CreateMap<Entities.City, Models.CityWithoutPOIsDTO>();
+            CreateMap<Entities.City, Models.CityDto>();
+            
+        }
+    }
+}
---
+++ CityInfo.API/MapProfiles/POIProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace CityInfo.API.MapProfiles
+{
+    public class POIProfile : Profile
+    {
+        public POIProfile()
+        {
+            CreateMap<Entities.PointOfInterest, Models.PointOfInterestDto>();
+        }
+    }
+}
